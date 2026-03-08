99d195a9ac5b9085662e065700fa7073a6cdb7f9
Tigran Zakaryan <t.zakaryan92@gmail.com>
2026-03-08 20:39:33 +0400

    Initial Commit

---
+++ QuickShot/QuickShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickShotApp.swift
+//  QuickShot
+//
+//  Created by Тигран Закарян on 08.03.26.
+//
+
+import SwiftUI
+
+@main
+struct QuickShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
